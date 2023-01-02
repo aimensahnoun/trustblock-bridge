2f7f23c02656f3d68578b15188aff698c8834a4a
Aimen Sahnoun <aimensahnoun@outlook.com>
2023-01-02 15:53:58 +0300

    feat: add deploye script for bridge contract

--- apps/hardhat/scripts/deploy.ts
+++ apps/hardhat/scripts/deploy.ts
@@ -1,18 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const BridgeFactory = await ethers.getContractFactory("Bridge");
+  const Bridge = await BridgeFactory.deploy();
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await Bridge.deployed();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log("Bridge deployed to:", Bridge.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
