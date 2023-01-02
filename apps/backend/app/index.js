6934549b138d3b1b954efdc879bbabab6ad1e3c6
Aimen Sahnoun <aimensahnoun@outlook.com>
2023-01-02 14:42:46 +0300

    fix: update docker-compose to link redis and nodejs script

--- apps/backend/app/index.js
+++ apps/backend/app/index.js
@@ -2,22 +2,19 @@ const Queue = require("bull");
 
 const eventQueue = new Queue("events", {
   redis: {
-    port: process.env.REDIS_PORT,
+    port: parseInt(process.env.REDIS_PORT),
     host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
   },
 });
 
 eventQueue.add({ name: "Aimen", age: 23 });
 
 eventQueue.process(function (job, done) {
+  console.log(job.data);
+
   job.progress(42);
 
   done();
-
-  done(new Error("error transcoding"));
-
-  done(null, { samplerate: 48000 /* etc... */ });
 
   throw new Error("some unexpected error");
 });
